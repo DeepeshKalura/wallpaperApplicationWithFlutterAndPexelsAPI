b1070406756e6c201927d7a5c581acbdeccd64f9
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-09-23 20:40:26 +0530

    create search screen

---
+++ lib/views/screens/search_screen.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+import '../../controller/api_controller.dart';
+import '../../model/photos_model.dart';
+import '../widgets/custom_app_bar.dart';
+import '../widgets/search_bar.dart';
+import './full_screen.dart';
+
+class SearchScreen extends StatefulWidget {
+  final String query;
+  const SearchScreen({super.key, required this.query});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  late List<PhotosModel> searchResults;
+  bool isLoading = true;
+  getSearchResults() async {
+    searchResults = await ApiController.searchWallpapers(widget.query);
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getSearchResults();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        title: const CustomAppBar(
+          word1: "Wallpaper",
+          word2: "Guru",
+        ),
+      ),
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: SearchBar(),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    height: MediaQuery.of(context).size.height,
+                    child: GridView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              mainAxisExtent: 400,
+                              crossAxisCount: 2,
+                              crossAxisSpacing: 13,
+                              mainAxisSpacing: 10),
+                      itemCount: searchResults.length,
+                      itemBuilder: ((context, index) {
+                        return GridTile(
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => FullScreen(
+                                    imgUrl: searchResults[index].imgSrc,
+                                  ),
+                                ),
+                              );
+                            },
+                            child: Hero(
+                              tag: searchResults[index].imgSrc,
+                              child: Container(
+                                height: 800,
+                                width: 50,
+                                decoration: BoxDecoration(
+                                  color: Colors.amberAccent,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: Image.network(
+                                      height: 800,
+                                      width: 50,
+                                      fit: BoxFit.cover,
+                                      searchResults[index].imgSrc),
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                    ),
+                  )
+                ],
+              ),
+            ),
+    );
+  }
+}
