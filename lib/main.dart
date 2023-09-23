4a8b5a0e89b9f368b008aeaa20e2446f85583ad6
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-09-23 20:22:09 +0530

    loadinf env file and directed to homescreen

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import './views/screens/home.dart';
+
+Future<void> main() async {
+  await dotenv.load(fileName: "assets/.env");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wallpaper Guru',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
